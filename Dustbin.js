3714e2c56e03a0802dff541517405872587a65e3
ADITYASANKARSANPADHY <76484698+ADITYASANKARSANPADHY@users.noreply.github.com>
2021-01-25 23:27:19 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -1,26 +1,32 @@
 class Dustbin{
-    constructor(){
-       // boxleftSprite=createSprite(560, 558, 15,70);
-       // boxleftSprite.shapeColor=color(255,0,0);
+    constructor(x,y){
+       
+       this.x= x;
+       this.y= y;
+       this.dustbinWidth=200;
+       this.dustbinHeight=100;
+       this.wallThickness=20;
+       this.angle=0;	
+
+        this.boxLeftBody = Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness,{isStatic:true} );
+        World.add(world,this.boxLeftBody);
     
-        boxLeftBody = Bodies.rectangle(x,y,width,height,options );
-        World.add(world, boxLeftBody);
+      
     
-       /* boxBase=createSprite(615, 585, 100,15);
-        boxBase.shapeColor=color(255,0,0);*/
+        this. boxBottomBody = Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness,{isStatic:true} );
+        World.add(world, this.boxBottomBody);
     
-        boxBottomBody = Bodies.rectangle(x,y,width,height,options );
-        World.add(world, boxBottomBody);
+        
     
-        /*boxrightSprite=createSprite(665 , 558, 15,70);
-        boxrightSprite.shapeColor=color(255,0,0);*/
-    
-        boxRightBody = Bodies.rectangle(x,y,width,height,options);
-        World.add(world, boxRightBody);
+        this.boxRightBody = Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness,{isStatic:true});
+        World.add(world, this.boxRightBody);
     
     }
 
     display(){
+        var pp=this.boxLeftBody.position;
+			var pp=this.boxBottomBody.position;
+			var pp=this.boxRightBody.position;
         push();
         translate(pp.x,pp.y);
         rectMode(CENTER);
